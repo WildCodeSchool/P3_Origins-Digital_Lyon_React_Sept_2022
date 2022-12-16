a8a259ded39c8d1ea759006b265b8e828fb0bdb5
AlouaneMohamedAmine <mohamedaminealouane88@gmail.com>
2022-12-16 14:37:23 +0100

    just for commit

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,10 +1,25 @@
-import Navbar from "@components/Navbar";
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 
 export default function SearchPage() {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <h1>Search</h1>
+      <h1>Recherche</h1>
+      <input
+        type="text"
+        placeholder="Que souhaitez vous regarder"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <h2>Parcourir tout</h2>
+      <div>
+        {/* Il va y avoir le grid ici avec toutes les catégories */}
+        <section>Catégory ya zebbi</section>
+        <section>Catégory ya zebbi</section>
+        <section>Catégory ya zebbi</section>
+        <section>Catégory ya zebbi</section>
+      </div>
       <div className="navPageContainer">
         <Navbar />
       </div>
