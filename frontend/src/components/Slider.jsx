1522fd5c854843493d5e20fba2c1d1b33f6dac11
SixcoH <samy.dumas69100@gmail.com>
2022-12-16 14:09:13 +0100

    add new composent slider on main page

---
+++ frontend/src/components/Slider.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Slider() {
+  return (
+    <div>
+      <h2>Name</h2>
+
+      <div className="slider-container">
+        <div className="slider-wrapper">
+          <div className="slider-item">ITEM 01</div>
+          <div className="slider-item">ITEM 02</div>
+          <div className="slider-item">ITEM 03</div>
+          <div className="slider-item">ITEM 04</div>
+          <div className="slider-item">ITEM 05</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
