fc02631c8ff7dde5d874c37764c98f62c6172f88
IdirX69 <adil.idirr@gmail.com>
2023-01-20 23:51:42 +0100

    video manager

--- frontend/src/components/VideosTable.jsx
+++ frontend/src/components/VideosTable.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
 import CurrentUserContext from "../../contexts/userContext";
 import ReturnPageButton from "./ReturnPageButton";
+import VideoBox from "./VideoBox";
 
 export default function UsersTable() {
   const [videosList, setVideosList] = useState([]);
   const { token } = useContext(CurrentUserContext);
+  const [search, setSearch] = useState("");
 
   const myHeaders = new Headers({
     Authorization: `Bearer ${token}`,
@@ -16,7 +18,7 @@ export default function UsersTable() {
     headers: myHeaders,
   };
 
-  const deleteVideos = (id) => {
+  const deleteVideo = (id) => {
     fetch(`http://localhost:5000/api/videos/${id}`, DELETErequestOptions);
   };
 
@@ -26,37 +28,76 @@ export default function UsersTable() {
       .then((videos) => setVideosList(videos));
   }, []);
   return (
-    <div>
+    <div className="video-table-container">
       <ReturnPageButton />
-      <h3>Liste Vidéos</h3>
-      <table>
-        <tbody>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Supprimer</th>
-          </tr>
-          {videosList.map((video) => {
-            return (
-              <tr key={video.id}>
-                <td>{video.id}</td>
-                <td>{video.name}</td>
-                <td>
-                  <button
-                    type="button"
-                    className="admin"
-                    onClick={() => {
-                      deleteVideos(video.id);
-                    }}
-                  >
-                    Supprimer
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <h3>Liste vidéos</h3>
+      <input
+        className="search-video"
+        type="text"
+        placeholder="Rechercher un utilisateur"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {search !== "" ? (
+        <div className="video-table">
+          {videosList
+            .filter((video) => video.name.toLowerCase().includes(search))
+
+            .map((video) => (
+              <div className="video-list">
+                <ul key={video.id} className="video-info">
+                  <VideoBox video={video} className="video-box-manage" />
+                </ul>
+                <ul className="video-manage">
+                  <li>#{video.id}</li>
+
+                  <li>
+                    {!video.is_admin ? (
+                      <button
+                        type="button"
+                        onClick={() => {
+                          deleteVideo(video.id);
+                        }}
+                      >
+                        Supprimer
+                      </button>
+                    ) : null}
+                  </li>
+                </ul>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div className="video-table">
+          <div>
+            {videosList.map((video) => {
+              return (
+                <div className="video-list">
+                  <ul key={video.id} className="video-info">
+                    <VideoBox video={video} className="video-box-manage" />
+                  </ul>
+                  <ul className="video-manage">
+                    <li>#{video.id}</li>
+
+                    <li>
+                      {!video.is_admin ? (
+                        <button
+                          type="button"
+                          onClick={() => {
+                            deleteVideo(video.id);
+                          }}
+                        >
+                          Supprimer
+                        </button>
+                      ) : null}
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
