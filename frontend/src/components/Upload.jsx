690a510878c88e2a8652d852eed9f7ca6c85a74b
IdirX69 <adil.idirr@gmail.com>
2023-01-20 23:12:48 +0100

    Form upload css

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -80,64 +80,70 @@ function Upload() {
   };
 
   return (
-    <div className="upload-container">
-      <ReturnPageButton />
-      <h2>
-        <strong> Upload De Vidéo</strong>
-      </h2>
-      <form encType="multipart/form-data" onSubmit={handleSubmit}>
-        <div className="form-group file-area">
-          <label htmlFor="video" className="form-label">
-            Vidéo
-          </label>
-          <input type="file" ref={videoRef} id="video" required="required" />
-          <div className="file-dummy">
-            <div className="success">Votre fichier a bien été sélectionnée</div>
-            <div className="default">Sélectionner une vidéo</div>
+    <>
+      <div className="upload-container">
+        <ReturnPageButton />
+        <h2>
+          <strong> Upload De Vidéo</strong>
+        </h2>
+        <form encType="multipart/form-data" onSubmit={handleSubmit}>
+          <div className="form-group file-area">
+            <label htmlFor="video" className="form-label">
+              Vidéo
+            </label>
+            <input type="file" ref={videoRef} id="video" required="required" />
+            <div className="file-dummy">
+              <div className="success">
+                Votre fichier a bien été sélectionnée
+              </div>
+              <div className="default">Sélectionner une vidéo</div>
+            </div>
           </div>
-        </div>
-        <div className="form-group file-area">
-          <label htmlFor="img" className="form-label">
-            Image
-          </label>
-          <input type="file" ref={imgRef} id="img" required="required" />
-          <div className="file-dummy">
-            <div className="success">Votre fichier a bien été sélectionnée</div>
-            <div className="default">Sélectionner une image</div>
+          <div className="form-group file-area">
+            <label htmlFor="img" className="form-label">
+              Image
+            </label>
+            <input type="file" ref={imgRef} id="img" required="required" />
+            <div className="file-dummy">
+              <div className="success">
+                Votre fichier a bien été sélectionnée
+              </div>
+              <div className="default">Sélectionner une image</div>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="name" className="form-label">
-            Nom de la vidéo
-          </label>
-          <input
-            onChange={(e) => setName(e.target.value)}
-            type="text"
-            id="name"
-            className="form-controll"
-            required="required"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description" className="form-label">
-            description
-          </label>
-          <textarea
-            onChange={(e) => setDescription(e.target.value)}
-            id="description"
-            className="form-controll "
-            required="required"
-          />
-        </div>
-        <div className="form-group">
-          <button className="containerbtn" type="submit">
-            Télécharger
-          </button>
-        </div>
-        <ToastContainer />
-      </form>
-      <Navbar />
-    </div>
+          <div className="form-group">
+            <label htmlFor="name" className="form-label">
+              Nom de la vidéo
+            </label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              id="name"
+              className="form-controll"
+              required="required"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description" className="form-label">
+              description
+            </label>
+            <textarea
+              onChange={(e) => setDescription(e.target.value)}
+              id="description"
+              className="form-controll "
+              required="required"
+            />
+          </div>
+          <div className="form-group">
+            <button className="containerbtn" type="submit">
+              Télécharger
+            </button>
+          </div>
+        </form>
+        <Navbar />
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 
