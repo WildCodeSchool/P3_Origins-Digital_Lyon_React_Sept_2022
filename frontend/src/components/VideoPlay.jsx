2a65adf76e742d2cd46496cc8151a656f931fc4b
Dreyrk <lucas.rondepierre123@gmail.com>
2023-01-20 15:19:57 +0100

    blabla

--- frontend/src/components/VideoPlay.jsx
+++ frontend/src/components/VideoPlay.jsx
@@ -19,7 +19,7 @@ function VideoPlay() {
   return (
     <div className="video-play-container">
       <Player
-        poster={`http://localhost:5000/api/videos/${videoPlayed.img}`}
+        autoPlay
         height={250}
         width={300}
         type="video/mp4"
