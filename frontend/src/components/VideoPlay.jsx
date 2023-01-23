7d7a1c79f44681efcea16a92710e14240cd0b50d
Dreyrk <lucas.rondepierre123@gmail.com>
2023-01-23 11:46:39 +0100

    category linked

--- frontend/src/components/VideoPlay.jsx
+++ frontend/src/components/VideoPlay.jsx
@@ -1,53 +1,44 @@
-// /* eslint-disable react/prop-types */
-// import axios from "axios";
-// import React, { useContext, useState, useEffect } from "react";
-// import { Player } from "video-react";
-// import CurrentVideosContext from "../../contexts/videosContext";
+/* eslint-disable react/prop-types */
+import axios from "axios";
+import React, { useContext, useState, useEffect } from "react";
+import { Player } from "video-react";
+import CurrentVideosContext from "../../contexts/videosContext";
 
-// function VideoPlay({}) {
-//   const { selectedName, selectedId, videoDate } =
-//     useContext(CurrentVideosContext);
+function VideoPlay({ video }) {
+  const { videoDate } = useContext(CurrentVideosContext);
 
-//   const [category, setCategory] = useState([]);
-//   const [videoPlayed, setVideoPlayed] = useState([]);
+  const backUrl = import.meta.env.VITE_BACKEND_URL;
 
-//   useEffect(() => {
-//     axios
-//       .get(`${backUrl}/api/videos/infos/${selectedId}`)
-//       .then((response) => {
-//         setVideoPlayed(response.data);
-//       })
-//       .catch((err) => console.error(err));
-//   }, []);
+  const [category, setCategory] = useState({});
 
-//   useEffect(() => {
-//     axios
-//       .get(`${backUrl}/api/category/${categoryId}`)
-//       .then((response) => {
-//         setCategory(response.data);
-//       })
-//       .catch((err) => console.error(err));
-//   }, []);
+  useEffect(() => {
+    axios
+      .get(`${backUrl}/api/category/${video.category_id}`)
+      .then((response) => {
+        setCategory(response.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-//   return (
-//     <div className="video-play-container">
-//       <Player
-//         autoPlay
-//         height={250}
-//         width={300}
-//         type="video/mp4"
-//         src={`http://localhost:5000/api/videos/${selectedName}`}
-//       />
-//       <h2>{videoPlayed.name}</h2>
-//       <p className="date-video">{videoDate(videoPlayed)}</p>
-//       <p className="video-description">{videoPlayed.description}</p>
-//       <div className="interaction">
-//         <div className="category-play">
-//           <h3>{category.name}</h3>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className="video-play-container">
+      <Player
+        autoPlay
+        height={250}
+        width={300}
+        type="video/mp4"
+        src={`http://localhost:5000/api/videos/${video.url}`}
+      />
+      <h2>{video.name}</h2>
+      <p className="date-video">{videoDate(video)}</p>
+      <p className="video-description">{video.description}</p>
+      <div className="interaction">
+        <div className="category-play">
+          <h3>{category.name}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default VideoPlay;
+export default VideoPlay;
--- frontend/src/pages/VideoPlayer.jsx
+++ frontend/src/pages/VideoPlayer.jsx
@@ -1,37 +1,46 @@
-// /* eslint-disable react/prop-types */
-// import React, { useContext, useEffect, useState } from "react";
-// import axios from "axios";
-// import Header from "../components/Header";
-// import Navbar from "../components/Navbar";
-// import Slider from "../components/Slider";
-// import Comment from "../components/Comment";
-// import VideoPlay from "../components/VideoPlay";
-// import CurrentVideosContext from "contexts/videosContext";
+/* eslint-disable react/prop-types */
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import CurrentVideosContext from "../../contexts/videosContext";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
+import Comment from "../components/Comment";
+import VideoPlay from "../components/VideoPlay";
 
-// function VideoPlayer() {
-//   const { selectedId } = useContext(CurrentVideosContext);
+function VideoPlayer() {
+  const { selectedId } = useContext(CurrentVideosContext);
 
-//   const backUrl = import.meta.env.VITE_BACKEND_URL;
+  const backUrl = import.meta.env.VITE_BACKEND_URL;
 
-//   const [category, setCategory] = useState([]);
+  const [videoPlayed, setVideoPlayed] = useState([]);
 
-//   const comment = {
-//     author: {
-//       name: "John Doe",
-//       avatar: "../../src/asset/image/user.svg",
-//     },
-//     message: "This is a great video!",
-//   };
+  useEffect(() => {
+    axios
+      .get(`${backUrl}/api/videos/infos/${selectedId}`)
+      .then((response) => {
+        setVideoPlayed(response.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-//   return (
-//     <div className="player-page">
-//       <Header />
-//       <Navbar />
-//       <VideoPlay />
-//       <Slider />
-//       <Comment author={comment.author} message={comment.message} />
-//     </div>
-//   );
-// }
+  const comment = {
+    author: {
+      name: "John Doe",
+      avatar: "../../src/asset/image/user.svg",
+    },
+    message: "This is a great video!",
+  };
 
-// export default VideoPlayer;
+  return (
+    <div className="player-page">
+      <Header />
+      <Navbar />
+      <VideoPlay video={videoPlayed} />
+      <Slider />
+      <Comment author={comment.author} message={comment.message} />
+    </div>
+  );
+}
+
+export default VideoPlayer;
