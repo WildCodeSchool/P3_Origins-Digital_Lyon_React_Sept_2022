d9f5df21877ef55ff51e401e3e12a6edafdddcba
SixcoH <samy.dumas69100@gmail.com>
2023-01-23 10:55:13 +0100

    push for checkout

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Favorites from "../components/Favorites";
 import Playlists from "../components/Playlists";
@@ -6,11 +7,20 @@ import ReturnPageButton from "../components/ReturnPageButton";
 
 function Library() {
   return (
-    <div className="libraryContainer">
-      <ReturnPageButton />
-      <h1>My Library</h1>
-      <div className="libraryPage">
-        <Favorites />
+    <div>
+      <div className="libraryContainer">
+        <ReturnPageButton />
+        <h1>My Library</h1>
+        <div className="libraryPage">
+          <div>
+            <Favorites />
+            <Link to="/favorites">
+              <button className="btn " type="button">
+                View All
+              </button>
+            </Link>
+          </div>
+        </div>
         <Playlists />
       </div>
       <Navbar />
