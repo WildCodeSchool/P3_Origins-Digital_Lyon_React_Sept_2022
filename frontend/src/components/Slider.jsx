935cf4c8e10e4fa4765710dec7519aef1e3a0dff
Rachid FAIK <rachidfaik1985@gmail.com>
2023-01-25 16:02:43 +0100

    adding links to categories

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import DefaultImageSlider from "../asset/4.jpg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Slider() {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  const [videosSlider, setVideosSlider] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/api/videos/promote`)
+      .then((response) => {
+        setVideosSlider(response.data);
+      })
+      .catch((err) => console.error(err));
+  }, [setVideosSlider]);
   return (
     <div>
       <h2>Category</h2>
 
       <div className="slider-container">
         <div className="slider-wrapper">
-          <img className="slider-item" src={DefaultImageSlider} alt="img1" />
-          <img className="slider-item" src={DefaultImageSlider} alt="img1" />
-          <img className="slider-item" src={DefaultImageSlider} alt="img1" />
-          <img className="slider-item" src={DefaultImageSlider} alt="img1" />
-          <img className="slider-item" src={DefaultImageSlider} alt="img1" />
+          {videosSlider.map((video) => (
+            <img
+              key={video.id}
+              className="slider-item"
+              src={`${BACKEND_URL}/api/videos/${video.img}`}
+              alt="imgOfSlider"
+            />
+          ))}
         </div>
       </div>
     </div>
